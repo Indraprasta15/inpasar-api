0268239c12b5acfc80cc3244e8aa54d55f6a29d5
Indra Prasetyo <idraprasetya@gmail.com>
2023-07-08 12:51:29 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const db = require("./models/bundle.model");
+
+app.use("/public", express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+db.sequelize.sync({ force: false });
+
+const produkRouter = require("./routes/produk");
+app.use("/produk", produkRouter);
+
+const kategoriRouter = require("./routes/kategori");
+app.use("/kategori", kategoriRouter);
+
+module.exports = app;
